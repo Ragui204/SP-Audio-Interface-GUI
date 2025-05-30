200c0a9f42f1bdb3e85e57fcab394756187a0ac5
Rolando Aguirre <78000107+Ragui204@users.noreply.github.com>
2025-05-29 20:50:13 -0700

    Add files via upload

---
+++ firmware/midi/drum_hex_codes/HatSamples.h
@@ -0,0 +1,39 @@
+// Audio data converted from WAV file by wav2sketch
+
+extern const unsigned int AudioSampleHatsmassamollawav[3777];
+// Audio data converted from WAV file by wav2sketch
+
+extern const unsigned int AudioSampleHatsmetalwav[2209];
+// Audio data converted from WAV file by wav2sketch
+
+extern const unsigned int AudioSampleHatsmicrowav[4097];
+// Audio data converted from WAV file by wav2sketch
+
+extern const unsigned int AudioSampleHatsnoisewav[1313];
+// Audio data converted from WAV file by wav2sketch
+
+extern const unsigned int AudioSampleHatspedalwav[4225];
+// Audio data converted from WAV file by wav2sketch
+
+extern const unsigned int AudioSampleHatssaltywav[3361];
+// Audio data converted from WAV file by wav2sketch
+
+extern const unsigned int AudioSampleHatssizzlewav[1505];
+// Audio data converted from WAV file by wav2sketch
+
+extern const unsigned int AudioSampleHatsspringwaterwav[9025];
+// Audio data converted from WAV file by wav2sketch
+
+extern const unsigned int AudioSampleHatsstutterwav[7841];
+// Audio data converted from WAV file by wav2sketch
+
+extern const unsigned int AudioSampleHatssweetwav[2465];
+// Audio data converted from WAV file by wav2sketch
+
+extern const unsigned int AudioSampleHatsvinylwav[5473];
+// Audio data converted from WAV file by wav2sketch
+
+extern const unsigned int AudioSampleHatswonkywav[5953];
+// Audio data converted from WAV file by wav2sketch
+
+extern const unsigned int AudioSampleHatszippowav[3073];
---
+++ firmware/midi/drum_hex_codes/SnareSamples.h
@@ -0,0 +1,108 @@
+// Audio data converted from WAV file by wav2sketch
+
+extern const unsigned int AudioSampleSnare1982drivewavf[3041];
+// Audio data converted from WAV file by wav2sketch
+
+extern const unsigned int AudioSampleSnareanalogwav[3585];
+// Audio data converted from WAV file by wav2sketch
+
+extern const unsigned int AudioSampleSnareblackoutwav[2945];
+// Audio data converted from WAV file by wav2sketch
+
+extern const unsigned int AudioSampleSnarebreathewav[3393];
+// Audio data converted from WAV file by wav2sketch
+
+extern const unsigned int AudioSampleSnarecassettewav[2689];
+// Audio data converted from WAV file by wav2sketch
+
+extern const unsigned int AudioSampleSnareclappersdelightwavf[6465];
+// Audio data converted from WAV file by wav2sketch
+
+extern const unsigned int AudioSampleSnarecrushedwav[5409];
+// Audio data converted from WAV file by wav2sketch
+
+extern const unsigned int AudioSampleSnaredatasettewav[2113];
+// Audio data converted from WAV file by wav2sketch
+
+extern const unsigned int AudioSampleSnaredoublebarrelwava[6817];
+// Audio data converted from WAV file by wav2sketch
+
+extern const unsigned int AudioSampleSnareduotonewav[1473];
+// Audio data converted from WAV file by wav2sketch
+
+extern const unsigned int AudioSampleSnareeightoheightwavfil[3489];
+// Audio data converted from WAV file by wav2sketch
+
+extern const unsigned int AudioSampleSnarefroggerwav[2977];
+// Audio data converted from WAV file by wav2sketch
+
+extern const unsigned int AudioSampleSnaregnrwav[4705];
+// Audio data converted from WAV file by wav2sketch
+
+extern const unsigned int AudioSampleSnarehypemachinewava[5025];
+// Audio data converted from WAV file by wav2sketch
+
+extern const unsigned int AudioSampleSnarejetwav[8929];
+// Audio data converted from WAV file by wav2sketch
+
+extern const unsigned int AudioSampleSnarelastbarwav[14465];
+// Audio data converted from WAV file by wav2sketch
+
+extern const unsigned int AudioSampleSnarelofiwav[2977];
+// Audio data converted from WAV file by wav2sketch
+
+extern const unsigned int AudioSampleSnarenineohninewavvfi[3841];
+// Audio data converted from WAV file by wav2sketch
+
+extern const unsigned int AudioSampleSnarenudiscowavw[5217];
+// Audio data converted from WAV file by wav2sketch
+
+extern const unsigned int AudioSampleSnareoffgridwav[3873];
+// Audio data converted from WAV file by wav2sketch
+
+extern const unsigned int AudioSampleSnareogwav[3073];
+// Audio data converted from WAV file by wav2sketch
+
+extern const unsigned int AudioSampleSnarepapercutwav[3521];
+// Audio data converted from WAV file by wav2sketch
+
+extern const unsigned int AudioSampleSnarepiccolowav[2369];
+// Audio data converted from WAV file by wav2sketch
+
+extern const unsigned int AudioSampleSnareretrowav[1665];
+// Audio data converted from WAV file by wav2sketch
+
+extern const unsigned int AudioSampleSnareslammerwav[7777];
+// Audio data converted from WAV file by wav2sketch
+
+extern const unsigned int AudioSampleSnaresmoochiewav[5281];
+// Audio data converted from WAV file by wav2sketch
+
+extern const unsigned int AudioSampleSnaresnapperwav[2241];
+// Audio data converted from WAV file by wav2sketch
+
+extern const unsigned int AudioSampleSnaresuctionwav[3617];
+// Audio data converted from WAV file by wav2sketch
+
+extern const unsigned int AudioSampleSnaretapestopwav[5857];
+// Audio data converted from WAV file by wav2sketch
+
+extern const unsigned int AudioSampleSnaretightwav[2401];
+// Audio data converted from WAV file by wav2sketch
+
+extern const unsigned int AudioSampleSnaretrappedwav[2081];
+// Audio data converted from WAV file by wav2sketch
+
+extern const unsigned int AudioSampleSnareurbanwav[2561];
+// Audio data converted from WAV file by wav2sketch
+
+extern const unsigned int AudioSampleSnareussrwav[5185];
+// Audio data converted from WAV file by wav2sketch
+
+extern const unsigned int AudioSampleSnareverbotronwav[6145];
+// Audio data converted from WAV file by wav2sketch
+
+extern const unsigned int AudioSampleSnarevhswav[8545];
+// Audio data converted from WAV file by wav2sketch
+
+extern const unsigned int AudioSampleSnarevinylwav[3905];
