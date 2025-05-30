83c90a12bf41bf0ff5fc1f1f001a0eddbecbffa8
Rolando Aguirre <78000107+Ragui204@users.noreply.github.com>
2025-05-29 20:38:07 -0700

    added drum hex code

---
+++ firmware/MIDI/KickSamples.h
@@ -0,0 +1,111 @@
+// Audio data converted from WAV file by wav2sketch
+
+extern const unsigned int AudioSampleKick1985wav[2689];
+// Audio data converted from WAV file by wav2sketch
+
+extern const unsigned int AudioSampleKickasacschraderwavf[6497];
+// Audio data converted from WAV file by wav2sketch
+
+extern const unsigned int AudioSampleKickboilerwav[15873];
+// Audio data converted from WAV file by wav2sketch
+
+extern const unsigned int AudioSampleKickborefest2021wavf[2561];
+// Audio data converted from WAV file by wav2sketch
+
+extern const unsigned int AudioSampleKickchipsynthwav[4897];
+// Audio data converted from WAV file by wav2sketch
+
+extern const unsigned int AudioSampleKickcoffeeshopwavv[3905];
+// Audio data converted from WAV file by wav2sketch
+
+extern const unsigned int AudioSampleKickdarkroomwav[5313];
+// Audio data converted from WAV file by wav2sketch
+
+extern const unsigned int AudioSampleKickdevastatewav[5121];
+// Audio data converted from WAV file by wav2sketch
+
+extern const unsigned int AudioSampleKickdusterwav[1953];
+// Audio data converted from WAV file by wav2sketch
+
+extern const unsigned int AudioSampleKickedgywav[4097];
+// Audio data converted from WAV file by wav2sketch
+
+extern const unsigned int AudioSampleKickfistpumpwav[4033];
+// Audio data converted from WAV file by wav2sketch
+
+extern const unsigned int AudioSampleKickglitcherwav[4289];
+// Audio data converted from WAV file by wav2sketch
+
+extern const unsigned int AudioSampleKickgrandmasterwav[5217];
+// Audio data converted from WAV file by wav2sketch
+
+extern const unsigned int AudioSampleKickhardwav[3137];
+// Audio data converted from WAV file by wav2sketch
+
+extern const unsigned int AudioSampleKickitwav[2337];
+// Audio data converted from WAV file by wav2sketch
+
+extern const unsigned int AudioSampleKickjuicywav[3777];
+// Audio data converted from WAV file by wav2sketch
+
+extern const unsigned int AudioSampleKickjunowav[2529];
+// Audio data converted from WAV file by wav2sketch
+
+extern const unsigned int AudioSampleKickloridawav[9377];
+// Audio data converted from WAV file by wav2sketch
+
+extern const unsigned int AudioSampleKicklowrezwavw[22081];
+// Audio data converted from WAV file by wav2sketch
+
+extern const unsigned int AudioSampleKickmechawav[4065];
+// Audio data converted from WAV file by wav2sketch
+
+extern const unsigned int AudioSampleKicknailgunwav[3777];
+// Audio data converted from WAV file by wav2sketch
+
+extern const unsigned int AudioSampleKickneatowav[3201];
+// Audio data converted from WAV file by wav2sketch
+
+extern const unsigned int AudioSampleKickoceanbreezewavf[3297];
+// Audio data converted from WAV file by wav2sketch
+
+extern const unsigned int AudioSampleKickpotswav[3329];
+// Audio data converted from WAV file by wav2sketch
+
+extern const unsigned int AudioSampleKickquakewav[2785];
+// Audio data converted from WAV file by wav2sketch
+
+extern const unsigned int AudioSampleKickretrowav[1697];
+// Audio data converted from WAV file by wav2sketch
+
+extern const unsigned int AudioSampleKickroundaboutwav[2465];
+// Audio data converted from WAV file by wav2sketch
+
+extern const unsigned int AudioSampleKickrummmmblewav[5121];
+// Audio data converted from WAV file by wav2sketch
+
+extern const unsigned int AudioSampleKickshowerwav[2945];
+// Audio data converted from WAV file by wav2sketch
+
+extern const unsigned int AudioSampleKickshowstopperwav[3073];
+// Audio data converted from WAV file by wav2sketch
+
+extern const unsigned int AudioSampleKickslowlowwavfil[28033];
+// Audio data converted from WAV file by wav2sketch
+
+extern const unsigned int AudioSampleKicksonyawav[4993];
+// Audio data converted from WAV file by wav2sketch
+
+extern const unsigned int AudioSampleKickswooshywav[5025];
+// Audio data converted from WAV file by wav2sketch
+
+extern const unsigned int AudioSampleKicktekkwav[3233];
+// Audio data converted from WAV file by wav2sketch
+
+extern const unsigned int AudioSampleKickthumpsterwav[5185];
+// Audio data converted from WAV file by wav2sketch
+
+extern const unsigned int AudioSampleKicktightwav[2145];
+// Audio data converted from WAV file by wav2sketch
+
+extern const unsigned int AudioSampleKicktronwav[6401];
